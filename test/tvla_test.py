a71686d1fffb96835f76efa8ebeb9472b9c14b84
Andreas Kurth <adk@lowrisc.org>
2022-06-30 12:26:47 +0200

    Add test for general TVLA on given histograms
    This adds two tests: each test takes a histogram as input, runs TVLA on
    it, and checks whether the resulting t-test traces contains a
    significant deviation from the mean, which would indicate leakage.  For
    one histogram we expect leakage, for the other we do not.
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- test/tvla_test.py
+++ test/tvla_test.py
@@ -2,6 +2,8 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+import numpy as np
+
 from .cmd import Args
 from .repo import RepoCmd
 
@@ -17,3 +19,24 @@ def test_help():
     tvla = TvlaCmd(Args('--help')).run()
     # Assert that a message is printed on stdout or stderr.
     assert(len(tvla.stdout()) != 0 or len(tvla.stderr()) != 0)
+
+
+def ttest_significant(ttest_trace) -> bool:
+    """Determine if a t-test trace contains a significant deviation from the mean."""
+    mean = np.mean(ttest_trace, axis=3)
+    stddev = np.std(ttest_trace, axis=3)
+    threshold = mean + 4.5 * stddev
+    abs_max = np.max(np.abs(ttest_trace), axis=3)
+    return np.any(abs_max > threshold)
+
+
+def test_general_leaking_histogram():
+    tvla = TvlaCmd(Args('-g -i test/data/tvla_general/sha3_hist_leaking')).run()
+    assert ttest_significant(np.load('tmp/ttest.npy')), (
+           f"{tvla} did not find significant leakage, which is unexpected")
+
+
+def test_general_nonleaking_histogram():
+    tvla = TvlaCmd(Args('-g -i test/data/tvla_general/sha3_hist_nonleaking')).run()
+    assert not ttest_significant(np.load('tmp/ttest.npy')), (
+           f"{tvla} did find significant leakage, which is unexepcted")
